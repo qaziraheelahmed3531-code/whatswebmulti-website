58430857fe8ed39035bc66e0f8608e7658d4b50a
qaziraheelahmed3531-code <qaziraheelahmed3531@gmail.com>
2026-06-05 00:44:40 +0500

    WhatsWebMulti Website v1.0

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      }
-    }
+    port: 5174
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    sourcemap: false
   }
 })
